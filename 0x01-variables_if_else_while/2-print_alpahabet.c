8c98a03aa5d1e9c4f3b55f6004b12607f1348ed7
Morenikeji-1 <keji.mb.kb@gmail.com>
2023-06-26 15:10:22 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alpahabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+
+int main(void)
+{
+	char a;
+	char z;
+
+	while (a < z)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar("\n");
+	return (0)
+}
