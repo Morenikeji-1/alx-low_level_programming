0f6c6bfd2461e04b6923833b2e7d3c5705647507
Morenikeji-1 <keji.mb.kb@gmail.com>
2023-06-27 07:34:33 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase excluding q and e
+ *
+ * Return:0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+			i++;
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
