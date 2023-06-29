71b0d3fbbef20fe0a68c8cb87dd9e89395c91238
Morenikeji-1 <keji.mb.kb@gmail.com>
2023-06-29 06:19:44 -0700

    printcomb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - a program that prints three digits followed by , and space
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j;
+	int k;
+
+	while (i <= '9')
+	{
+		j = '0';
+		while (j <= '9')
+		{
+			k = '0';
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+
+	}
+	putchar('\n');
+	return (0);
+}
