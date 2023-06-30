f3adc36c36637f0a614a81b2916daf82bec26311
Morenikeji-1 <keji.mb.kb@gmail.com>
2023-06-30 15:08:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - A program that prints alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar('i');
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
