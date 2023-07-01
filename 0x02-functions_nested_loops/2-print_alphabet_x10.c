62da6d979a05b38cfa735d79f87d0fbc60494305
Morenikeji-1 <keji.mb.kb@gmail.com>
2023-07-01 01:13:20 -0700

    2-alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char k;
+	char j;
+
+	for (k = 0; k <= 10; k++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
+
